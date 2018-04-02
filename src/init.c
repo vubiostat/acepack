38552da6c076e8093c4853c9ec3140a5675ab137
Shawn Garbett <shawn.garbett@vanderbilt.edu>
2018-04-02 09:48:14 -0500

    Added new routine registration code

---
+++ src/init.c
@@ -0,0 +1,19 @@
+#include <R_ext/RS.h>
+#include <stdlib.h> // for NULL
+#include <R_ext/Rdynload.h>
+
+/* .Fortran calls */
+extern void F77_NAME(avas)(void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *);
+extern void F77_NAME(mace)(void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *);
+
+static const R_FortranMethodDef FortranEntries[] = {
+    {"avas", (DL_FUNC) &F77_NAME(avas), 16},
+    {"mace", (DL_FUNC) &F77_NAME(mace), 14},
+    {NULL, NULL, 0}
+};
+
+void R_init_acepack(DllInfo *dll)
+{
+    R_registerRoutines(dll, NULL, NULL, FortranEntries, NULL);
+    R_useDynamicSymbols(dll, FALSE);
+}
