b3b785a6f67964d2bd7cd7a8a29024959135eb8f
Shawn Garbett <shawn.garbett@vumc.org>
2025-02-06 18:56:58 -0600

    Added automated test #11

---
+++ tests/testthat/test_ace_test.R
@@ -0,0 +1,29 @@
+context("ACE Permutation Test")
+
+
+test_that("Test repeats known results for n=200",
+{
+  set.seed(1)
+  
+  n <- 200
+  x <- matrix(rnorm(n*2), n)
+  nu <- 2
+  y <- x / sqrt(rchisq(n, nu)/nu) #multivariate t
+
+  expect_no_error(x <- ace.test(y))
+  
+  expect_equal(x$n, 999)
+  expect_equal(x$ace, 0.5614263, tolerance=1e-6)
+  expect_equal(x$pval, 0.001,    tolerance=1e-6)
+  
+  n <- 5
+  x <- matrix(rnorm(n*2), n)
+  nu <- 2
+  y <- x / sqrt(rchisq(n, nu)/nu) #multivariate t
+
+  expect_no_error(x <- ace.test(y))
+  
+  expect_equal(x$n, 120)
+  expect_equal(x$ace, 0.7654646, tolerance=1e-6)
+  expect_equal(x$pval, 9/99,     tolerance=1e-6)
+})
